f01cc070288820f446449e9ba88765ec68ff7214
SagonaAchhami <np03cs4s250102@heraldcollege.edu.np>
2026-02-01 11:33:14 +0545

    updated

--- config/db.php
+++ config/db.php
@@ -1,8 +1,22 @@
 <?php
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+    // Localhost config
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "recipe_db";
+} else {
+    // Server config
+    $host = "localhost";
+    $user = "np03cs4s250102";
+    $pass = "4pOTFRQ1f3";
+    $db   = "np03cs4s250102";
+}
+
 $pdo = new PDO(
-    "mysql:host=localhost;dbname=recipe_db;charset=utf8mb4",
-    "root",
-    "",
+    "mysql:host=$host;dbname=$db;charset=utf8mb4",
+    $user,
+    $pass,
     [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
